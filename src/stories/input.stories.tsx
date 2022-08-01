4e60a79d4459d75feeb3baff22ae6525c8efa53b
irina-snezan <irina-snezan@tut.by>
2022-08-01 16:19:29 +0300

    fixed input stories

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -58,7 +58,7 @@ export const ControlledCheckbox = () => {
     }
     return <input type='checkbox' checked={parentValue} onChange={onChangeHandler}/>
 }
-export const ControlledCSelect = () => {
+export const ControlledSelect = () => {
     const [parentValue, setParentValue] = useState<string | undefined>(undefined)
     const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
         setParentValue(e.currentTarget.value)
