0c175301c1c0a89616702cd71513d6e59f9e8596
irina-snezan <irina-snezan@tut.by>
2022-08-01 15:20:36 +0300

    add story of controlled input and checkbox and select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -43,3 +43,30 @@ export const GetValueOfUncontrolledInputByButtonPress2 = () => {
         <button onClick={save}>save</button>
         - {value} </>
 }
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type='checkbox' checked={parentValue} onChange={onChangeHandler}/>
+}
+export const ControlledCSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChangeHandler}>
+        <option>none</option>
+        <option value={"1"}>Minsk</option>
+        <option value={"2"}>Moscow</option>
+        <option value={"3"}>Kiev</option>
+    </select>
+}
