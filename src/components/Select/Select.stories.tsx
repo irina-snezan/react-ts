a4836de344e16a8c8ab1700ed5ac161d324757a4
irina-snezan <irina-snezan@tut.by>
2022-08-04 01:17:26 +0300

    add select

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import {ComponentMeta} from '@storybook/react';
+import {Select} from './Select';
+import {action} from '@storybook/addon-actions';
+import {ComponentStory} from '@storybook/react';
+
+export default {
+    title: 'Select',
+    component: Select,
+} as ComponentMeta<typeof Select>;
+
+export const WithValue = () => {
+    const [value, setValue] = useState('2')
+
+    return <>
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {value: 1, title: 'Home'},
+                    {value: 2, title: 'About us'},
+                    {value: 3, title: 'Contacts'},
+                ]}/>
+    </>
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState('null');
+    return <>
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {value: 1, title: 'Home'},
+                    {value: 2, title: 'About us'},
+                    {value: 3, title: 'Contacts'},
+                ]}/>
+    </>
+}
+
+
+// const callback = action('accordion mode change event fired');
+// const onClickCallback = action('some item was clicked');
+//export const Primary: ComponentStory<typeof Select> = () => <Select primary>Select</Select>;
+// export const Template: Story<SelectPropsType> = (args) => <Select {...args} />
+// const callbacksProps = {
+//     onChange: callback
+//     //onClick: onclickCallback
+// }
+//
+// export const Select = Template.bind({});
+// Select.args = {
+//     ...callbacksProps,
+//     titleValue: 'Country',
+//     //collapsed: false,
+//     items: [{value: 1, title: 'Belarus'}, {value: 2, title: 'Italy'}, {value: 3, title: 'China'}],
+//
+// };
+// export const ModeChanging: Story<SelectPropsType> = (args) => {
+//     const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+//
+//     const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+//         setParentValue(e.currentTarget.value)
+//     }
+//     return <Select {...args} onChange={onChangeHandler} onClick={(value:any) => alert(`this point of menu with ID ${value}`}
+//                              items: { [
+//                                 {value: 1, title:'Home'},
+//                                 {value: 2, title:'About us'},
+//                                 {value: 3, title:'Contacts'}
+//                             ]} />
+// };
+//
+// ModeChanging.args = {
+//     titleValue: 'Country',
+//     items: [{value: 1, title: 'Home'}, {value: 2, title: 'About us'}, {value: 3, title: 'Contacts'}],
+// }
+
+
+// export const Select = () => {
+//     const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+//     const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+//         setParentValue(e.currentTarget.value)
+//     }
+//     return <select value={parentValue} onChange={onChangeHandler}>
+//         <div>none</div>
+//         <div value={'1'}>Belarus</div>
+//         <div value={'2'}>Italy</div>
+//         <div value={'3'}>China</div>
+//     </select>
+// }
